4165a0f6946e407ebf9bddb8f399cd8178c13e35
bakbak92 <bakarycoly92@gmail.com>
2017-06-21 15:05:27 +0200

    mise en place mvc

---
+++ view/accueil.php
@@ -0,0 +1,54 @@
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<!-- Indicators -->
+<ol class="carousel-indicators">
+  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+</ol>
+
+<!-- Wrapper for slides -->
+<div class="carousel-inner" role="listbox">
+  <div class="item active">
+    <img src="view/img/kid.jpg" alt="...">
+    <div class="carousel-caption">
+      ...
+    </div>
+  </div>
+  <div class="item">
+    <img src="view/img/kid2.jpg" alt="...">
+    <div class="carousel-caption">
+      ...
+    </div>
+  </div>
+  ...
+</div>
+
+<!-- Controls -->
+<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+<div class="container action">
+<h2>Nos actions</h2>
+<div class="col-md-3">
+  <img src="view/img/intervention.png" alt="">
+  <p>Les interventions dans les écoles ou institutions accueillant des enfants et adolescents</p>
+</div>
+<div class="col-md-3">
+  <img src="view/img/conférence.png" alt="">
+  <p>l'organisation de débat et de conférences</p>
+</div>
+<div class="col-md-3">
+  <img src="view/img/sensibilisation.png" alt="">
+  <p>l’organisation d’ateliers à destination des enfants et des adultes</p>
+</div>
+<div class="col-md-3">
+  <img src="view/img/pub.png" alt="">
+  <p>la réalisation de campagnes de communication et de sensibilisation</p>
+</div>
+</div>
---
+++ view/footer.php
@@ -0,0 +1,9 @@
+<footer class="container-fluid">
+  <ul class="social-media">
+    <li><a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+    <li><a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+  </ul>
+  <p>Copyright 2017 - Mentions légales - réalisé par bakary</p>
+</footer>
+</body>
+</html>
