59293ed12886bcb9aaee9209f3e1c111752e9762
bakbak92 <bakarycoly92@gmail.com>
2017-06-21 14:56:41 +0200

    modif chemin

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="#"><img src="img/logo-association.png" alt=""></a>
+          <a href="#"><img src="view/img/logo-association.png" alt=""></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -49,13 +49,13 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="img/kid.jpg" alt="...">
+      <img src="view/img/kid.jpg" alt="...">
       <div class="carousel-caption">
         ...
       </div>
     </div>
     <div class="item">
-      <img src="img/kid2.jpg" alt="...">
+      <img src="view/img/kid2.jpg" alt="...">
       <div class="carousel-caption">
         ...
       </div>
@@ -76,19 +76,19 @@
 <div class="container action">
   <h2>Nos actions</h2>
   <div class="col-md-3">
-    <img src="img/intervention.png" alt="">
+    <img src="view/img/intervention.png" alt="">
     <p>Les interventions dans les écoles ou institutions accueillant des enfants et adolescents</p>
   </div>
   <div class="col-md-3">
-    <img src="img/conférence.png" alt="">
+    <img src="view/img/conférence.png" alt="">
     <p>l'organisation de débat et de conférences</p>
   </div>
   <div class="col-md-3">
-    <img src="img/sensibilisation.png" alt="">
+    <img src="view/img/sensibilisation.png" alt="">
     <p>l’organisation d’ateliers à destination des enfants et des adultes</p>
   </div>
   <div class="col-md-3">
-    <img src="img/pub.png" alt="">
+    <img src="view/img/pub.png" alt="">
     <p>la réalisation de campagnes de communication et de sensibilisation</p>
   </div>
 </div>
