273a004f83de15a7961a0c6083a14ebac593a7b5
aymericgoube <aymericgoube@hotmail.fr>
2017-06-21 17:33:36 +0200

    page actualite

---
+++ view/actualite.php
@@ -0,0 +1,54 @@
+<?php require 'header.php' ?>
+<body>
+  <div class="article-container">
+    <h1>ACTUS</h1>
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+            <img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <a href="#" class="read-more">Lire plus</a>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+<img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+              </div>
+        </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+<img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+        </div>
+          </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+            <img src="img/eiffel.jpg" alt="">
+
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+        </div>
+        </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+              <img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+<?php require 'footer.php' ?>
