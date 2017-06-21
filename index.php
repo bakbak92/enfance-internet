caede83ae1fd89de0e21d010b33cafa8b5cfda62
bakbak92 <bakarycoly92@gmail.com>
2017-06-21 15:34:01 +0200

    modif structure

--- index.php
+++ index.php
@@ -1,17 +1,61 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="view/css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <title>Enfance & Internet</title>
-</head>
 
 <?php
 require 'view/header.php';
-require 'view/accueil.php';
+?>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="view/img/kid.jpg" alt="...">
+      <div class="carousel-caption">
+        <a href="#" class="a-propos">A propos</a>
+      </div>
+    </div>
+    <div class="item">
+      <img src="view/img/kid2.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="container action">
+  <h2>Nos actions</h2>
+  <div class="col-md-3">
+    <img src="view/img/intervention.png" alt="">
+    <p>Les interventions dans les écoles ou institutions accueillant des enfants et adolescents</p>
+  </div>
+  <div class="col-md-3">
+    <img src="view/img/conférence.png" alt="">
+    <p>l'organisation de débat et de conférences</p>
+  </div>
+  <div class="col-md-3">
+    <img src="view/img/sensibilisation.png" alt="">
+    <p>l’organisation d’ateliers à destination des enfants et des adultes</p>
+  </div>
+  <div class="col-md-3">
+    <img src="view/img/pub.png" alt="">
+    <p>la réalisation de campagnes de communication et de sensibilisation</p>
+  </div>
+</div>
+<?php
 require 'view/footer.php'
- ?>
+?>
--- view/header.php
+++ view/header.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="view/css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <title>Enfance & Internet</title>
+</head>
 <body>
   <header>
     <nav class="navbar navbar-default my-nav">
@@ -16,9 +27,17 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav-right">
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Qui sommes nous<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">L'origine</a></li>
+                <li><a href="#">L'équipe</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Nos actions</a></li>
+            <li><a href="#">Presse</a></li>
+            <li><a href="#">Actualité</a></li>
             <li class="dropdown">
               <a href="#" class="don">Faite un Don</a>
             </li>
