ca341576a89ed95a338487f15f96608d29138c06
bakbak92 <bakarycoly92@gmail.com>
2017-06-21 17:32:42 +0200

    modif page d'accueil

--- index.php
+++ index.php
@@ -15,16 +15,21 @@ require 'view/header.php';
     <div class="item active">
       <img src="view/img/kid.jpg" alt="...">
       <div class="carousel-caption">
-        <a href="#" class="a-propos">A propos</a>
+        <div class="intro">
+          <p>Créée en février 2017, l’association Enfance et Internet est une association à but non lucratif</p>
+          <p><a href="#" class="a-propos">A propos</a></p>
+        </div>
       </div>
     </div>
     <div class="item">
       <img src="view/img/kid2.jpg" alt="...">
       <div class="carousel-caption">
-        ...
+        <div class="intro">
+          <p>Nos missions principales la protection, la préservation et l’éducation des enfants et adolescents sur Internet</p>
+          <p><a href="#" class="a-propos">A propos</a></p>
+        </div>
       </div>
     </div>
-    ...
   </div>
 
   <!-- Controls -->
@@ -37,7 +42,7 @@ require 'view/header.php';
     <span class="sr-only">Next</span>
   </a>
 </div>
-<div class="container action">
+<div class="container-fluid action">
   <h2>Nos actions</h2>
   <div class="col-md-3">
     <img src="view/img/intervention.png" alt="">
@@ -55,6 +60,58 @@ require 'view/header.php';
     <img src="view/img/pub.png" alt="">
     <p>la réalisation de campagnes de communication et de sensibilisation</p>
   </div>
+</div>
+<div class="container last-actualité">
+  <h2>Nos dernières actualités</h2>
+  <div class="col-md-4">
+    <img src="view/img/image1.jpg" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+  <div class="col-md-4">
+    <img src="view/img/image2.jpg" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+  <div class="col-md-4">
+    <img src="view/img/image3.jpg" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+  </div>
+</div>
+<div class="container-fluid adhesion">
+  <h2>Adhérer</h2>
+  <div class="col-md-4">
+    <img src="view/img/membre.png" alt="">
+    <h3>Devenez bénévole</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <a href="#" class="btn">Devenir bénévole</a>
+  </div>
+  <div class="col-md-4">
+    <img src="view/img/don.png" alt="">
+    <h3>Faire un don</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <a href="#" class="btn">Faites un don</a>
+  </div>
+  <div class="col-md-4">
+    <img src="view/img/partenaire.png" alt="">
+    <h3>Devenez partenaire</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <a href="#" class="btn">Devenir partenaire</a>
+  </div>
+</div>
+<div class="container partenaire">
+<h2>Nos partenaire</h2>
+<div class="col-md-4">
+  <img src="view/img/partenaire1.jpg" alt="">
+</div>
+<div class="col-md-4">
+<img src="view/img/partenaire2.jpg" alt="">
+</div>
+<div class="col-md-4">
+  <img src="view/img/partenaire3" alt="">
+</div>
 </div>
 <?php
 require 'view/footer.php'
